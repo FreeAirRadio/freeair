58d15a2f280c25d5936c80cea235d4c5667db44d
Ksenyadk <koshechka259@mail.ru>
2015-02-03 18:12:47 +0300

    Added diagram for block of popularity

--- js/main.js
+++ js/main.js
@@ -205,6 +205,67 @@ function scrollnews(){
   $('#b-news').horizontalScroll();
 }
 
+function diagram(){
+  $(function () {
+    $('#container').highcharts({
+        chart: {
+            zoomType: 'x'
+        },
+        title: {
+            text: 'Popularity'
+        },
+        xAxis: {
+            type: 'datetime',
+        },
+        yAxis: {
+            title: {
+                text: null
+            }
+        },
+
+        labels: {
+            align: 'left',
+            format: '{value:.0f}'
+        },
+
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                },
+                marker: {
+                    radius: 2
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
+
+        series: [{
+            type: 'area',
+            name: 'votes',
+            pointInterval: 31 * 24 * 3600 * 1000,
+            pointStart: Date.UTC(2014, 0),
+            data: [
+                3300, 2000, 4100, 3150, 4000, 3150, 4500, 3285, 3750, 3850, 2650
+            ]
+        }]
+    });
+});
+}
+
 $(document).ready(function(){
   stickyHeader();
 
@@ -227,6 +288,8 @@ $(document).ready(function(){
   clickly();
 
   scrollnews();
+
+  diagram();
 });
 
 $(window).resize(function(){
